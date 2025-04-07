724ae2e4879248b08497cba32caaa4e6b5e144d4
Eva <evaayuaan@gmail.com>
2025-04-07 18:41:20 +0100

    add createAccount component

---
+++ src/app/components/createAccount/createAccount.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-createAccount',
+  imports: [RouterModule, CommonModule],
+  templateUrl: './createAccount.component.html',
+  styleUrl: './createAccount.component.css'
+})
+export class CreateAccountComponent {
+
+}
