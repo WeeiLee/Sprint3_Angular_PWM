945a969430fca4c59763b3b726b026190e5f75cc
crazy <crazydoufuz@gmail.com>
2025-04-20 17:57:53 +0100

    modified configuration

---
+++ src/app/components/message-audio/message-audio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-audio',
+  imports: [],
+  templateUrl: './message-audio.component.html',
+  styleUrl: './message-audio.component.css'
+})
+export class MessageAudioComponent {
+
+}
