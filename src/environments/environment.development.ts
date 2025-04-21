b40b471828a4e6a244ed2c2033e08b0136b7a7d8
crazy <crazydoufuz@gmail.com>
2025-04-21 17:08:22 +0100

    add firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,13 @@
-export const environment = {};
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCQYUmvQptNikyoBaqcPnJgzJVw33yiB3M",
+    authDomain: "pwm-whisp.firebaseapp.com",
+    projectId: "pwm-whisp",
+    storageBucket: "pwm-whisp.firebasestorage.app",
+    messagingSenderId: "607876297093",
+    appId: "1:607876297093:web:cfc2e855edece2808254ec",
+    measurementId: "G-SD6D0RVQSY"
+  }
+};
